ed946ac38d463d7d1fe5896c16c0f33706f77fb0
adahe8 <ada.he0908@gmail.com>
2025-11-15 23:39:48 -0800

    Wrote form.js script and linked to HTML to assist with dynamic (real time, informative) validation, customizing error messages with Validation API for the email and comments fields, and appling error styling when user leaves (unfocuses) form field filled in improperly

--- form.js
+++ form.js
@@ -1,2 +1,45 @@
-const requiredElements = document.querySelectorAll('[required]');
-E
+const fullname = document.getElementById("name");
+const email = document.getElementById("email");
+const linkedin = document.getElementById("linkedin");
+const comments = document.getElementById("comments");
+// get all inputs
+const inputs = document.querySelectorAll("input, textarea");
+
+// want to make required field go red after input field interacted with if left empty
+// first, if form is focused, remove any specialized invalid styling
+inputs.forEach((field) => {
+  field.addEventListener("focus", function () {
+    field.classList.remove("invalid");
+  });
+});
+// track interaction - if field touched and then left, then have validation display
+inputs.forEach((field) => {
+  field.addEventListener("blur", function () {
+    if (!field.checkValidity()) {
+      field.classList.add("invalid");
+      field.reportValidity();
+    } else {
+      field.classList.remove("invalid");
+      field.reportValidity();
+    }
+  });
+});
+
+// setting specific validity messages
+email.addEventListener("input", (event) => {
+  if (!email.checkValidity() && email.validity.typeMismatch) {
+    email.setCustomValidity(
+      "Enter in a proper email address of the form name@domain.com."
+    );
+  } else {
+    email.setCustomValidity("");
+  }
+});
+
+comments.addEventListener("input", (event) => {
+  if (!comments.checkValidity()) {
+    comments.setCustomValidity("A comment is required to submit this form.");
+  } else {
+    comments.setCustomValidity("");
+  }
+});
